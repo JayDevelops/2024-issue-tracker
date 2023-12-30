a5ad8e403ead136102df53d59858c44bf4dc44be
Jay.develops <jaymations1234@gmail.com>
2023-12-30 15:13:11 -0800

    Show the issues on a table in issues/page.tsx

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,56 @@
-import react from "react"
-import {Button} from "@/components/ui/button";
-import Link from "next/link";
-const IssuesPage = () => {
+import { buttonVariants } from "@/components/ui/button"
+import Link from "next/link"
+import prisma from "@/prisma/client"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
+
+const IssuesPage = async () => {
+    // Retrieve the issues from the DB
+    const issues = await prisma.issue.findMany()
+    const newIssuePageRoute: string = "/issues/new"
+
     return (
         <div>
-            <Link href="/issues/new">
-                <Button variant="default">New Issue</Button>
-            </Link>
+            <div className="mb-5">
+                <Link className={buttonVariants({ variant: "primaryBlue" })} href={newIssuePageRoute}>Click here</Link>
+            </div>
+
+            {/*Parent divs add border and adjust the table within that border*/}
+            <div className="rounded-md border">
+                <div className="relative w-full overflow-auto">
+                    <Table>
+                        <TableCaption>List of all recorded issues.</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Issue</TableHead>
+                                <TableHead className="hidden md:table-cell">Status</TableHead>
+                                <TableHead className="hidden md:table-cell">Created</TableHead>
+                            </TableRow>
+                        </TableHeader>
+
+                        <TableBody>
+                            {issues.map((issue) => (
+                                <TableRow key={issue.id}>
+                                    <TableCell>
+                                        {issue.title}
+                                        <div className="block md:hidden">{issue.status}</div>
+                                    </TableCell>
+                                    <TableCell className="hidden md:table-cell">{issue.status}</TableCell>
+                                    <TableCell className="hidden md:table-cell">{issue.createdAt.toDateString()}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
         </div>
     )
 }
