f87a14692d77fbab8d24b1a6f2594b215d112823
Jay.develops <jaymations1234@gmail.com>
2023-12-29 20:04:40 -0800

    handled new issue form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,52 @@
 'use client'
 import {Button} from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import SimpleMDE from "react-simplemde-editor"
+import dynamic from 'next/dynamic'
+import axios from "axios"
+import {useForm, Controller} from "react-hook-form"
 import "easymde/dist/easymde.min.css"
+import {useRouter} from "next/navigation"
 
+interface IssueForm {
+    title: string,
+    description: string,
+}
 const NewIssuePage = () => {
+    //  Use the router to push the new route to the issues page after submission
+    const router = useRouter()
+
+    //  Needed to dynamically import to avoid forced server side rendering
+    const SimpleMDE = dynamic(() => import('react-simplemde-editor'),
+        {
+            ssr: false,
+            loading: () => <h3 className="text-primary text-2xl">Loading Markdown Editor...</h3>
+        })
+
+    // Destructure the register from react-hook-form
+    const {register, control, handleSubmit} = useForm<IssueForm>()
+
 
     return (
-        <div className="grid max-w-xl gap-1.5 space-y-3">
-            <Input name="title" id="title" placeholder="Title" />
+        <form
+            className="grid max-w-xl gap-1.5 space-y-3"
+            onSubmit={handleSubmit(async (data) => {
+                await axios.post('/api/issues', data)
+                router.push('/issues')
+            })}
+        >
+            <Input placeholder="Title" {...register('title')}  />
 
-            <SimpleMDE placeholder="Write Your Issue Description Here..."/>
+            {/* react-hook-form controls the field on the simplemde editor, to retrieve that input*/}
+            <Controller
+                name="description"
+                control={control}
+                render={ ( {field} ) => (
+                    <SimpleMDE placeholder="Write Your Issue Description Here..." {...field}/>
+                )}
+            />
 
-            <Button variant="primaryBlue" className="w-1/4" type="submit">Submit New Issue</Button>
-        </div>
+            <Button variant="primaryBlue" className="w-1/4">Submit New Issue</Button>
+        </form>
     )
 }
 
