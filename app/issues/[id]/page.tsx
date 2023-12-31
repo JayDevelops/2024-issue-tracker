5dedb91bf1fa08aa50d7c78b27f159f58da0dd99
Jay.develops <jaymations1234@gmail.com>
2023-12-30 21:29:43 -0800

    add the edit button in [id]/page.tsx, also configured layout

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -5,6 +5,9 @@ import IssueStatusBadge from "@/components/IssueStatusBadge"
 import {Text} from "@/components/typography/Text"
 import {Card} from "@/components/ui/card"
 import ReactMarkdown from "react-markdown"
+import {Button} from "@/components/ui/button"
+import {Pencil2Icon} from "@radix-ui/react-icons"
+import Link from "next/link";
 
 interface IssueDetailPageProps {
     params: {id: string}
@@ -22,19 +25,28 @@ const IssueDetailPage = async ({params}: IssueDetailPageProps) => {
     }
 
     return (
-        <div>
-            <HeadingOneH1>{issue.title}</HeadingOneH1>
-            <div className="flex flex-row space-x-3">
-                <Text>{issue.createdAt.toDateString()}</Text>
-                <IssueStatusBadge status={issue.status} />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+                <HeadingOneH1>{issue.title}</HeadingOneH1>
+                <div className="flex flex-row space-x-3">
+                    <Text>{issue.createdAt.toDateString()}</Text>
+                    <IssueStatusBadge status={issue.status}/>
+                </div>
+
+                <Card className="my-4">
+                    <ReactMarkdown className="prose prose-stone dark:prose-invert p-3">
+                        {issue.description}
+                    </ReactMarkdown>
+
+                </Card>
             </div>
 
-            <Card className="my-4">
-                <ReactMarkdown className="prose prose-stone dark:prose-invert p-3">
-                    {issue.description}
-                </ReactMarkdown>
+            <div className="md:mt-0">
+                <Button asChild variant="primaryBlue">
+                    <Link href={`/issues/${issue.id}/edit`}> <Pencil2Icon /> &nbsp; Edit Issue</Link>
+                </Button>
 
-            </Card>
+            </div>
         </div>
     )
 }
