a1f68ad76ca551d390f445cbb2334d973123f19e
Jay.develops <jaymations1234@gmail.com>
2024-01-01 14:37:03 -0800

    Removed unused toast in Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,8 @@
-"use client"
-import {Button} from "@/components/ui/button"
-
-import { ToastAction } from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
 
 export default function Home() {
-    const { toast } = useToast()
-  return (
+    return (
       <div>
-          <Button
-              variant="outline"
-              onClick={() => {
-                  toast({
-                      title: "Scheduled: Catch up ",
-                      description: "Friday, February 10, 2023 at 5:57 PM",
-                      action: (
-                          <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
-                      ),
-                  })
-              }}
-          >
-              Add to calendar
-          </Button>
+          Home Page
       </div>
   )
 }
