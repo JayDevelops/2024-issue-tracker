fdb3884ce5f5e37491ffc41495a3a53fc8f3098e
Jay.develops <jaymations1234@gmail.com>
2024-01-02 21:34:18 -0800

    handled setAssignee method to update on the database

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -10,9 +10,13 @@ import {Issue, User} from "@prisma/client"
 import axios from "axios"
 import {useQuery} from "react-query"
 import Skeleton from "@/components/Skeleton/Skeleton"
+import { useToast } from "@/components/ui/use-toast"
+import {ToastAction} from "@/components/ui/toast"
 
 
 const AssigneeSelect = ({issue}: {issue: Issue}) => {
+    const { toast } = useToast()
+
     //  User react query to retrieve the user data, then handle errors and isLoading states.
     const {data: users, error, isLoading} = useQuery<User[]>({
         queryKey: ["users"],
@@ -27,11 +31,27 @@ const AssigneeSelect = ({issue}: {issue: Issue}) => {
     // If error occurs three times then return null
     if(error) return null
 
+    //  handleSetAssignee() checks if the user sets an unassigned value or to a different user,
+    //  then updates on the database
+    const handleSetAssignee = async (userId: string) => {
+        //  Check if the userId is unassigned then set to unassigned, else set to the passed userId string
+        const assignToUser = userId === "unassigned" ? null : userId;
+
+        //  Patch (update) the issue to the userId or null if unassigned
+        await axios
+            .patch("/api/issues/" + issue.id, { assignedToUserId: assignToUser })
+            .catch(() => {
+                toast({
+                    variant: "destructive",
+                    title: "Something went wrong.",
+                    description: "Changes could not be saved",
+                    action: <ToastAction altText="Try again">Try again</ToastAction>,
+                })
+            })
+    }
+
     return (
-        <Select defaultValue={issue.assignedToUserId || "unassigned"} onValueChange={(userId) => {
-            const assignToUser = userId === "unassigned" ? null : userId
-            axios.patch("/api/issues/" + issue.id, {assignedToUserId: assignToUser})
-        }}>
+        <Select defaultValue={issue.assignedToUserId || "unassigned"} onValueChange={handleSetAssignee}>
             <SelectTrigger className="w-full">
                 <SelectValue placeholder="Assign..." />
             </SelectTrigger>
@@ -47,4 +67,7 @@ const AssigneeSelect = ({issue}: {issue: Issue}) => {
         </Select>
     )
 }
+
+
 export default AssigneeSelect
+
