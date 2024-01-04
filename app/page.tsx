145cc79c2e536526a04ed44623f20d4ec4ae2f24
Jay.develops <jaymations1234@gmail.com>
2024-01-03 16:01:59 -0800

    Laid out the Dashboard page with IssueSummary, IssueChart, and LatestIssues

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
 import IssueSummary from "@/app/IssueSummary";
 import IssueChart from "@/app/IssueChart";
+import LatestIssues from "@/app/LatestIssues";
 
 export default async function Home() {
     //  Get each count for the three types of issues statuses
     const openCount = await prisma?.issue.count({
         where: {status: "OPEN"}
-    })
+    }) || 0
     const inProgressCount = await prisma?.issue.count({
         where: {status: "IN_PROGRESS"}
-    })
+    }) || 0
     const closedCount = await prisma?.issue.count({
         where: {status: "CLOSED"}
-    })
+    }) || 0
 
 
     return (
-        <IssueChart open={openCount || 0} inProgress={inProgressCount || 0} closed={closedCount || 0} />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <div className="flex flex-col gap-5">
+                <IssueSummary open={openCount} inProgress={inProgressCount} closed={closedCount} />
+                <IssueChart open={openCount} inProgress={inProgressCount} closed={closedCount} />
+            </div>
+            <LatestIssues />
+        </div>
   )
 }
