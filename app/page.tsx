632207b371045eed7244f4aecd2f0a19b1993009
Jay.develops <jaymations1234@gmail.com>
2024-01-18 20:30:49 -0800

    fixed dashboard page to make it dynamic, so it isn't static.

--- app/page.tsx
+++ app/page.tsx
@@ -8,27 +8,26 @@ export default async function Home() {
     //  Get each count for the three types of issues statuses
     const openCount = await prisma.issue.count({
         where: {status: "OPEN"}
-    }) || 0
+    })
     const inProgressCount = await prisma?.issue.count({
         where: {status: "IN_PROGRESS"}
-    }) || 0
+    })
     const closedCount = await prisma?.issue.count({
         where: {status: "CLOSED"}
-    }) || 0
-
-    // Here we will have one object to share between the IssueSummary and IssueChart
-    const sharedIssueProps = {open: openCount, inProgress: inProgressCount, closed: closedCount}
+    })
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             <div className="flex flex-col gap-5">
-                <IssueSummary {...sharedIssueProps} />
-                <IssueChart {...sharedIssueProps} />
+                <IssueSummary open={openCount} inProgress={inProgressCount} closed={closedCount}/>
+                <IssueChart open={openCount} inProgress={inProgressCount} closed={closedCount} />
             </div>
             <LatestIssues />
         </div>
   )
 }
+
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
     title: "Issue Tracker - Dashboard",
